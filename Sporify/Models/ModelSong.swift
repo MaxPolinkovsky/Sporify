dce34279a8b45b2fb4785b04f8cabc5c92e29ad8
Max Polinkovsky <max.polinkovsky@gmail.com>
2021-01-24 14:12:15 +0100

    add TableView and cell

---
+++ Sporify/Models/ModelSong.swift
@@ -0,0 +1,16 @@
+//
+//  ModelSong.swift
+//  Sporify
+//
+//  Created by Max Polinkovsky on 24.01.21.
+//
+
+import Foundation
+
+struct Song {
+    let name: String
+    let albumName: String
+    let artistName: String
+    let imageName: String
+    let trackName: String
+}
---
+++ Sporify/TracksView/SongCell.swift
@@ -0,0 +1,23 @@
+//
+//  SongCell.swift
+//  Sporify
+//
+//  Created by Max Polinkovsky on 24.01.21.
+//
+
+import UIKit
+
+class SongCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
