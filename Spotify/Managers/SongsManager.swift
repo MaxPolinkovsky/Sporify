1f1cf4c1ad14ff809e47f5b9a7e091126407c220
Max Polinkovsky <max.polinkovsky@gmail.com>
2021-02-24 14:30:27 +0100

    update Coredata

--- Spotify/Managers/SongsManager.swift
+++ Spotify/Managers/SongsManager.swift
@@ -10,37 +10,37 @@ import CoreData
 
 class SongsManager {
     
-   static let shared = SongsManager()
-   let songs = SPTManager.shared.testSongs
+    static let shared = SongsManager()
+    let songs = SPTManager.shared.testSongs
     
     //создание сущности
     func makeContext() {
         if let context = CoreDataManager.shared.managedContext, let _ = NSEntityDescription.entity(forEntityName: SongEntity.entityID, in: context){
-            var songsEntity = [Song]()
-            
-            for song in songs {
-                songsEntity.append(song)
-            }
-                do {
+            do {
+                for song in songs {
+                    let s = SongEntity(context: context)
+                    s.albumName = song.albumName
+                    s.artistName = song.artistName
+                    s.trackName = song.trackName
+                    s.imageName = song.imageName
                     try context.save()
-                } catch  {
-                    context.rollback()
-                    print(error.localizedDescription)
                 }
+            } catch {
+                context.rollback()
+                print(error.localizedDescription)
             }
         }
-
+    }
     //запрос данных
     func requestData() -> [Song] {
         if let context = CoreDataManager.shared.managedContext {
-            let request = NSFetchRequest<SongEntity>(entityName: SongEntity.entityID)
+            let request: NSFetchRequest<SongEntity> = SongEntity.fetchRequest()
             do {
-                let result = try context.fetch(request)
+                let result = try context.fetch(request) as [SongEntity]
                 let songs = result.map{$0.initSong()}
                 print("Songs: \(songs)")
                 return songs
-                
-            } catch  {
+            } catch {
                 print(error.localizedDescription)
             }
         }
